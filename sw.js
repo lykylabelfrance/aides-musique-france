330a68d674812d56928de7ee6b11ef71aa3e747f
lykylabelfrance <lykylabelfrance@gmail.com>
2026-03-26 23:40:43 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,55 @@
+var CACHE_NAME = 'aides-musique-v1';
+var urlsToCache = [
+  '/aides-musique-france/',
+  '/aides-musique-france/index.html',
+  '/aides-musique-france/aides.html',
+  '/aides-musique-france/eligibilite.html',
+  '/aides-musique-france/mentions-legales.html',
+  '/aides-musique-france/favicon.svg',
+  '/aides-musique-france/og-image.svg',
+  '/aides-musique-france/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        var responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      }).catch(function() {
+        return caches.match('/aides-musique-france/index.html');
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
